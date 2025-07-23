33a0920434243391793a2239ab89a777c4b6012a
hack-Csharp9 <alwaysthesame007@gmail.com>
2025-07-23 23:59:02 +0000

    Create helloword.py

---
+++ Jour01/helloword.py
@@ -0,0 +1,70 @@
+
+
+# 2)
+# Addition
+print(3 + 4)
+
+# Soustraction
+print(3 - 4)
+
+# Multiplication
+print(3 * 4)
+
+# Modulos
+print(3 % 4)
+
+# division
+print(3 / 4)
+
+# exponentiation
+print(3 ** 4)
+
+# division entière
+print(3 // 4)
+
+# 3)
+print("votre nom")
+print("votre nom de famille")
+
+print("votre pays")
+
+print("je profite de 30 jours")
+
+print("de python")
+
+# 4)
+print(10)
+
+print(9,8)
+
+print(3.14)
+
+print(4 - 4J)
+
+print( ['Asabeneh', 'Python', 'Finlande'] )
+
+print("Votre nom")
+
+print("votre nom de famille")
+
+print("votre pays")
+
+# 3)
+
+x = 10 # entier
+
+y = 3.14 # float
+
+z = complex(3, 4)  # complex 
+
+chaine = "hello world" # string
+
+var_bool = True # Booléen
+
+liste = [2,3,5] # list
+
+tuple1 = (1,2,3) # tuple
+
+set1 = {1,2,3} # set
+
+dict2 = {0: "moi", 1: "toi", 2: "elle"} #dictionnaire
