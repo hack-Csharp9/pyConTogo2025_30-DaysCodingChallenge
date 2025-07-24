8e393e0d4a19226d8fc1bcfe16d5e2f70ef77898
hack-Csharp9 <alwaysthesame007@gmail.com>
2025-07-24 23:25:17 +0000

    Update variables.py

--- Jour02/variables.py
+++ Jour02/variables.py
@@ -1,4 +1,5 @@
 # Day 2: 30 Days of python programming
+from math import*
 
 # Level 1
 first_name = "Lena"
@@ -24,6 +25,7 @@ is_light_on = False
 country_1, capital, is_on_earth, economic_rank = "USA", "Washington, D.C", True, 1
 
 # Level 2
+# 1)
 print(type(first_name))
 print(type(last_name))
 print(type(full_name))
@@ -38,10 +40,43 @@ print(type(country_1))
 print(type(capital))
 print(type(is_on_earth))
 print(type(economic_rank))
-
+# 2)
 print(len("Charbel"))
-
+# 3)
 print(len("Charbel") > len("KOUWADAN"))
+# 4)
+num_one, num_two = 5, 4
+# 5)
+total = num_two + num_one
+# 6)
+diff = num_two - num_one
+# 7)
+product = num_one * num_two
+# 8)
+division = num_one / num_two
+# 9)
+remainder = num_two % num_one
+# 10)
+exp = num_one**num_two
+# 11)
+floor_division = num_one // num_two
 
+# 12)
+# i)
+area = pi*(30**2)
+# ii)
+circum_of_circle = 2*pi*30
+# iii)
+radius = float(input("Enter the radius of the circle : "))
+user_area = pi*(radius**2)
+
+# 13)
+user_first_name = input("Enter your first name : ")
+user_last_name = input("Enter your last name : ")
+user_country = input("Enter your country name : ")
+age = int(input("Enter your age : "))
+
+# 14)
+help('keywords')
 
 
