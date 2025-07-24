3168e84d5767a32d57a1d8e054ab55aacd54bd2c
hack-Csharp9 <alwaysthesame007@gmail.com>
2025-07-24 23:02:05 +0000

    Create variables.py

---
+++ Jour02/variables.py
@@ -0,0 +1,47 @@
+# Day 2: 30 Days of python programming
+
+# Level 1
+first_name = "Lena"
+
+last_name = "STEINER"
+
+full_name = "Lena STEINER"
+
+country = "France"
+
+city = "Lausanne"
+
+age = 16
+
+year = 2025
+
+is_married = False
+
+is_true = True
+
+is_light_on = False
+
+country_1, capital, is_on_earth, economic_rank = "USA", "Washington, D.C", True, 1
+
+# Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(country_1))
+print(type(capital))
+print(type(is_on_earth))
+print(type(economic_rank))
+
+print(len("Charbel"))
+
+print(len("Charbel") > len("KOUWADAN"))
+
+
+
