d44e12c83883a9deb891c0e1fe47668a36a0292f
hack-Csharp9 <alwaysthesame007@gmail.com>
2025-07-26 16:45:46 +0000

    I don't know what to say

---
+++ Jour04/Exercises _by_Charbel_4.py
@@ -0,0 +1,19 @@
+# 1)
+space = ' '
+concatenate_string_1 = 'Thirty' + space + 'Days' + space + 'Of' + space + 'Python'
+print(concatenate_string_1)
+# 2)
+concatenate_string_2 = 'Coding' + space + 'For' + space + 'All'
+print(concatenate_string_2)
+# 3)
+company =  "Coding For All"
+# 4)
+print(company)
+# 5)
+print(len(company))
+# 6)
+all_caps_company = company.upper()
+print(all_caps_company)
+# 7)
+lowercase_company = all_caps_company.lower()
+print(lowercase_company)
