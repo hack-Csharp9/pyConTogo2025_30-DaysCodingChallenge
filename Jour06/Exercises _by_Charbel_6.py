24afc3f217fa950c92d12dc39829b95509ffdbb0
hack-Csharp9 <alwaysthesame007@gmail.com>
2025-07-28 23:25:25 +0000

    Create Exercises _by_Charbel_6.py

---
+++ Jour06/Exercises _by_Charbel_6.py
@@ -0,0 +1,20 @@
+### Level 1 :
+
+# 1)
+empty_tuple  = ()
+# 2)
+brothers_tuples = ('Jesus', 'Paul', 'Peter', 'James', 'John', 'Daniel', 'Israel', 'Joseph', 'David')
+sisters_tuples = ('Kelly', 'Gifty', 'Urielle', 'Priscilla', 'Ruth', 'Esther', 'Rose', 'Felicia', 'Damienne')
+# 3)
+siblings_tuples = brothers_tuples + sisters_tuples
+# 4)
+print(siblings_tuples)
+print(f"Numbers of siblings are : {len(siblings_tuples)}")
+# 5)
+parents_tuples = ('Abraham', 'Sarah')
+family_members = parents_tuples + siblings_tuples
+
+ ### Level 2 :
+
+ # 1)
+ 
