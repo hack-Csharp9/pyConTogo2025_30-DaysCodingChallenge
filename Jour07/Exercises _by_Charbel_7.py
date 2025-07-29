a8e1cdacbcae9c463c8770f3cd9043222888bf9c
hack-Csharp9 <alwaysthesame007@gmail.com>
2025-07-29 22:43:18 +0000

    Create Exercises _by_Charbel_7.py

---
+++ Jour07/Exercises _by_Charbel_7.py
@@ -0,0 +1,13 @@
+###  sets
+
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'} 
+A = {19, 22, 24, 20, 25, 26} 
+B = {19, 22, 20, 25, 26, 24, 28, 27} 
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+# Exercises: Level 1
+# 1)
+print(len(it_companies))
+# 2)
+it_companies.add('Twitter')
+
